5f8a8b2bd75c8aba449f68a4b563eca525c036e4
Dor <dorzairi@ymail.com>
2021-03-16 22:20:50 -0400

    Added a favicon and database init

--- controllers/newsFeed.js
+++ controllers/newsFeed.js
@@ -1,4 +1,5 @@
 const Community = require("../models/community");
+const User = require("../models/user");
 
 module.exports = {
   addPost
@@ -6,12 +7,20 @@ module.exports = {
 
 async function addPost(req, res, next) {
   try {
-    let post = await Community.findById(req.user._id);
+    await Community.create({
+      content: req.body.content_post,
+      user: req.user.id
+    });
 
-    console.log(req.body.content_post);
-    post[content].push(req.body.content_post);
+    console.log("Hey im logged at line 10 ");
+
+    let content = { content: req.body.content_post };
+    console.log("Hey im logged at line 13");
+
+    console.log(content);
+    res.redirect("/show");
   } catch (error) {
-    console.log(error);
+    console.log("The Error Is - " + error);
   }
 
   // console.log(req.body.value);
